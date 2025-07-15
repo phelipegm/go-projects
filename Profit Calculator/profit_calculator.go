39a50f949441b25db47eab4dce5117e530a35a7b
Phelipe Martins <phelipe.martins@koerber-supplychain.com>
2025-07-15 11:23:36 -0300

    handling errors practice

--- Profit Calculator/profit_calculator.go
+++ Profit Calculator/profit_calculator.go
@@ -1,25 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+	"os"
+)
+
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 	
 	fmt.Printf("EBT: %.1f\n", ebt)
 	fmt.Printf("Profit: %.1f\n", profit)
 	fmt.Printf("Ratio: %.1f\n", ratio)
+	storeResults(ebt, profit, ratio)
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("value must be positive")
+	}
+
+	return userInput, nil
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("Profit Calculator/results.txt", []byte(results), 0644)
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
