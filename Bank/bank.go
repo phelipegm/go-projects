55013d0301d7ae6508a0cf9069b084c4e6c189b5
Phelipe Martins <phelipe.martins@koerber-supplychain.com>
2025-07-15 12:04:50 -0300

    packages practice

--- Bank/bank.go
+++ Bank/bank.go
@@ -2,37 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
-	"errors"
+	"example.com/bank/fileops"
+	"github.com/Pallinder/go-randomdata"
 )
 
 const accountBalanceFile = "Bank/balance.txt"
 
-func getBalanceFromFile() (float64, error) {
-	data, err := os.ReadFile(accountBalanceFile)
-
-	if err != nil {
-		return 1000, errors.New("failed to find balance file")
-	}
-
-	balanceText := string(data)
-	balance, err := strconv.ParseFloat(balanceText, 64)
-
-	if err != nil {
-		return 1000, errors.New("failed to parse stored balance value")
-	}
-
-	return balance, nil
-}
-
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
-}
-
 func main() {
-	var accountBalance, err = getBalanceFromFile()
+	var accountBalance, err = fileops.GetFloatFromFile(accountBalanceFile)
 
 	if err != nil {
 		fmt.Println("ERROR")
@@ -42,14 +19,11 @@ func main() {
 	}
 
 	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("Today is", randomdata.Day())
 	
 	for {
-			fmt.Println("\nWhat do you want to do?")
-			fmt.Println("1. Check balance")
-			fmt.Println("2. Deposit money")
-			fmt.Println("3. Withdraw money")
-			fmt.Println("4. Exit")
-
+			presentOptions()
+			
 			var choice int
 			fmt.Print("\nYour choice: ")
 			fmt.Scan(&choice)
@@ -69,7 +43,7 @@ func main() {
 
 				accountBalance += depositAmout
 				fmt.Println("Balance updated! New amount:", accountBalance)
-				writeBalanceToFile(accountBalance)
+				fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
 			case 3:
 				fmt.Print("\nWithdrawal amount: ")
 				var withdrawalAmount float64
@@ -87,7 +61,7 @@ func main() {
 
 				accountBalance -= withdrawalAmount
 				fmt.Println("Balance updated! New amount:", accountBalance)
-				writeBalanceToFile(accountBalance)
+				fileops.WriteFloatToFile(accountBalance, accountBalanceFile)
 			default:
 				fmt.Println("\nGoodbye!")
 				return
